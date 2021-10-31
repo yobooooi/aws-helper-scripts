adcaf6aabf6c57f636f44614072a408f93490705
Adan Patience <yobooooi@users.noreply.github.com>
2021-11-01 00:25:38 +0200

    FEAT: added rich console outputs to the output

--- ec2-iam-users.py
+++ ec2-iam-users.py
@@ -1,7 +1,11 @@
 import boto3
 import os
 import json
+from rich import console
 
+from rich.console import Console
+from rich.table import Table
+from rich.progress import track
 #
 # Author: Adan Patience
 # Date: 24 November 2020
@@ -10,8 +14,17 @@ import json
 # UserId, UserName, AccessKeyId, LastUsedDate
 #
 #
-profile = 'synthesis-internal-dev'
-region =  'eu-west-1'
+profile = 'globee'
+region  =  'eu-west-1'
+
+console  = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("UserID", style="dim")
+table.add_column("UserName")
+table.add_column("AccessKeyId", justify="right")
+table.add_column("AccessKeyLastUsed", justify="right")
+
 
 if __name__ == '__main__':
     os.environ['AWS_SHARED_CREDENTIALS_FILE'] = '~/.aws/credentials'
@@ -24,9 +37,9 @@ if __name__ == '__main__':
     )
     paginator = iam.get_paginator('list_users') #get paginator resource for the list_aggregate_discovered_resources method
     page_iterator = paginator.paginate() #create paginator and parse parameters and iterate over results in for loop
-  
+
     for aggregated_resources in page_iterator:
-      for user in aggregated_resources['Users']:
+      for user in track(aggregated_resources['Users']):
         response_access_keys = iam.list_access_keys(
           UserName=user['UserName']
         )
@@ -35,7 +48,10 @@ if __name__ == '__main__':
             AccessKeyId=access_keys['AccessKeyId']
           )
           if 'LastUsedDate'in last_used['AccessKeyLastUsed']:
-            print(user['UserId'], end=",")
-            print(user['UserName'], end=",")
-            print(access_keys['AccessKeyId'], end=",")
-            print(last_used['AccessKeyLastUsed']['LastUsedDate'])
+            table.add_row(
+              "{0}".format(user['UserId']),
+              "[yellow]{0}[/yellow]".format(user['UserName']),
+              "{0}".format(access_keys['AccessKeyId']),
+              "{0}".format(last_used['AccessKeyLastUsed']['LastUsedDate'])
+            )
+    console.print(table)
